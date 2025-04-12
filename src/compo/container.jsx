ba2ce47b3d28140ca705298e30f37c6fcf986e6b
surajsingh9582 <143062308+surajsingh9582@users.noreply.github.com>
2025-04-12 12:11:05 +0530

    Update container.jsx

--- src/compo/container.jsx
+++ src/compo/container.jsx
@@ -19,7 +19,7 @@ let Container=()=>{
         }
     }
 
-    return <div className="h-110 border w-96 mx-auto mt-15 rounded-lg dark:bg-gray-800">
+    return <div className="h-110 border w-96 mx-auto mt-15 rounded-lg bg-black">
         <Searchbar value={data}/>
         <Btn arr={arr} data={Cdata}/>
     </div>
